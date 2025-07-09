0c9de5dbdbb5f0a6901224679202a2d04ec7ba8b
Takeda02092010 <kabiravohidova36@gmail.com>
2025-07-09 17:44:22 +0500

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:project1/cubit/auth_cubit.dart';
+import 'package:project1/cubit/auth_cubit.dart';   
 import 'package:project1/cubit/category_cubit.dart';
-import 'package:project1/cubit/v_c_cubit.dart';
+import 'package:project1/cubit/v_c_cubit.dart';    
 import 'package:project1/cubit/vendors_cubit.dart';
 import 'package:project1/lesson/page/home_page.dart';
-import 'package:project1/pages/auth/sign_in.dart';
+import 'package:project1/pages/auth/sign_in.dart';   
 import 'package:project1/pages/auth/sign_up.dart';
 import 'package:project1/pages/auth/verification_code.dart';
 import 'package:project1/pages/home/vendors.dart';
